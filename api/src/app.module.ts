bc6d9a307deb271470395cc35448cb6c795d5e81
DrZIn-dev <quick.car6020@fastmail.com>
2022-03-23 16:31:35 +0700

    feat(api/config): add database config and default value

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './pkg/config/database.config';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TodoModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    TodoModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ api/src/pkg/config/database.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT || 5432,
+  db: process.env.POSTGRES_DB || 'postgres',
+  username: process.env.POSTGRES_USERNAME || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '',
+  sync: String(process.env.POSTGRES_SYNC) === 'true',
+  drop: String(process.env.POSTGRES_DROP) === 'true',
+}));
