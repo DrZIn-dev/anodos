0b2c31b96e880a637523577d82e9c4f48b8bf90f
DrZIn-dev <quick.car6020@fastmail.com>
2022-03-23 16:24:44 +0700

    feat(api): env runtime configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [ConfigModule.forRoot(), TodoModule],
   controllers: [],
   providers: [],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port: number = +configService.get('PORT');
+  await app.listen(port, () => {
+    logger.log(`Listen on : ${port}`);
+  });
 }
 bootstrap();
